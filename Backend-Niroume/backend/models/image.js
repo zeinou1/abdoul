ab0609ebb0436a78993176415d0ef4c31db8766a
abdoulkader <zarikader@gmail.com>
2025-01-08 10:46:24 +0100

    update cv

--- Backend-Niroume/backend/models/image.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const imageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-}, { timestamps: true });
-
-const Image = mongoose.model("Image", imageSchema);
-export default Image;
